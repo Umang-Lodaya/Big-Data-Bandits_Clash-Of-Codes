c9c3a5f7e8cc0d361f6408839d273506152d51fd
UMANG KIRIT LODAYA <91935807+Umang-Lodaya@users.noreply.github.com>
2023-03-05 11:13:00 +0530

    Final Streamlit Commit

--- main.py
+++ main.py
@@ -28,13 +28,15 @@ from streamlit_card import card
 URI = "https://c3b4-2409-40c0-28-699f-41d3-899a-fa53-6435.in.ngrok.io/"
 
 def main():
+    st.sidebar.image("https://www.getidyll.in/assets/logo-1.svg")
+    # st.sidebar.title("IDLLY!")
     vert_space = "<body style='bgcolor: \"pink\";'></body>"
     st.markdown(vert_space, unsafe_allow_html=True)
     # st.sidebar.title("Navigation Bar")
-    user_menu=st.sidebar.radio('Select an Option', ('Home','Recommendations','Exploratory Data Analysis'))
+    user_menu=st.sidebar.radio('Select an Option', ('Home','Recommendations','Exploratory Data Analysis', 'Suggestions & Future Scope'))
 
     if(user_menu=='Home'):
-        st.title("PROFILE SCORING!")
+        st.header("FIND YOUR PERFECT MATCH!")
         st.markdown('Provide the follwing data')
         data = st.text_area("Data (json):", {})
         st.json(json.loads(data))
@@ -56,50 +58,81 @@ def main():
         
         col1, col2 = st.columns(2)
         col1.subheader("Drinking Habits")
-        col1.plotly_chart(px.pie(users, values='_id', names='is_habit_drink'), use_container_width=True)
+        col1.plotly_chart(px.pie(users, values='_id', names='is_habit_drink', color_discrete_sequence=["pink", "purple"]), use_container_width=True)
         
         col2.subheader("Smoking Habits")
-        col2.plotly_chart(px.pie(users, values='_id', names='is_habit_smoke'), use_container_width=True)
+        col2.plotly_chart(px.pie(users, values='_id', names='is_habit_smoke', color_discrete_sequence=["pink", "purple"]), use_container_width=True)
         
         
         col1, col2 = st.columns(2)
         col1.subheader("What to Find")
-        col1.plotly_chart(px.pie(users, values='_id', names='what_to_find'), use_container_width=True)
+        col1.plotly_chart(px.pie(users, values='_id', names='what_to_find', color_discrete_sequence=["pink", "purple"]), use_container_width=True)
         
         # vert_space = '<div style="padding: 30px 150px;"></div>'
         # st.markdown(vert_space, unsafe_allow_html=True)
 
         col2.subheader("Age")
-        col2.plotly_chart(px.line(users, x="_id", y="age"), use_container_width=True)
+        col2.plotly_chart(px.line(users, x="_id", y="age", color_discrete_sequence=["pink", "purple"]), use_container_width=True)
 
+    elif (user_menu == 'Suggestions & Future Scope'):
+        st.write('## Suggestions:')
+        st.write("""Using a drop down menu to enter college information (Improve the data collection and analysis).
+                    \n
+Should not be utilised as a loading animation (It can confuse the user and cross and like animation should be used for like and dislike)
+                    \n
+More female consumers should be drawn to the platform, since this will immediately boost the male audience.
+                    \n
+Displaying pop-ups with relationship advice (free user gets 3-5 pop up per day and we can ask them to take pro membership to avail more tips and more features)
+                    \n
+According to us, no insta id should be used because it might reduce the user's time and engagement.""")
+        
+        st.write("During our testing, we discovered a problem in which a female account received more likes than a free account and was also able to see who liked, despite the fact that they are intended to be pro features.")
+    
+        st.write('## Future Scope')
+        st.write("""We can apply multiple policies/algo for scoring and recommendations system and we can randomly use any policy and then record the feedback for it and using reinforcement learning do exploration and exploitation to select best policy""")
     else:
         st.header('Recommendations')
+
+
         col1, col2, col3, col4, col5 = st.columns(5)
-        col1.subheader("Verified Users")
-        col1.plotly_chart(px.pie(users, values='_id', names='is_subscribed', color_discrete_sequence=["pink", "purple"][::-1]), use_container_width=True)
         
-        col2.subheader("Subscribed Users")
-        col2.plotly_chart(px.pie(users, values='_id', names='is_subscribed', color_discrete_sequence=["pink", "purple"][::-1]), use_container_width=True)
-        col3.subheader("Subscribed Users")
-        col3.plotly_chart(px.pie(users, values='_id', names='is_subscribed', color_discrete_sequence=["pink", "purple"][::-1]), use_container_width=True)
-        col4.subheader("Subscribed Users")
-        col4.plotly_chart(px.pie(users, values='_id', names='is_subscribed', color_discrete_sequence=["pink", "purple"][::-1]), use_container_width=True)
-        col5.subheader("Subscribed Users")
-        col5.plotly_chart(px.pie(users, values='_id', names='is_subscribed', color_discrete_sequence=["pink", "purple"][::-1]), use_container_width=True)
+        for i in [0, 5]:
+            # col1.subheader("User " + str(i+1))
+            hasClicked = card(
+        title="User " + str(i+1),
+        text="Some description",
+        # image="http://placekitten.com/200/300",
+        url="https://github.com/gamcoh/st-card"
+        )
+            # col1.plotly_chart(px.pie(users, values='_id', names='is_subscribed', color_discrete_sequence=["pink", "purple"][::-1]), use_container_width=True)
+            # col2.subheader("User " + str(i+2))
+            hasClicked = card(title="User " + str(i+2), text="Some description",)
         
-        col1, col2, col3, col4, col5 = st.columns(5)
-        col1.subheader("Verified Users")
-        col1.plotly_chart(px.pie(users, values='_id', names='is_verified', color_discrete_sequence=["pink", "purple"]), use_container_width=True)
-        
-        col2.subheader("Subscribed Users")
-        col2.plotly_chart(px.pie(users, values='_id', names='is_subscribed', color_discrete_sequence=["pink", "purple"][::-1]), use_container_width=True)
-        col3.subheader("Subscribed Users")
-        col3.plotly_chart(px.pie(users, values='_id', names='is_subscribed', color_discrete_sequence=["pink", "purple"][::-1]), use_container_width=True)
-        col4.subheader("Subscribed Users")
-        col4.plotly_chart(px.pie(users, values='_id', names='is_subscribed', color_discrete_sequence=["pink", "purple"][::-1]), use_container_width=True)
-        col5.subheader("Subscribed Users")
-        col5.plotly_chart(px.pie(users, values='_id', names='is_subscribed', color_discrete_sequence=["pink", "purple"][::-1]), use_container_width=True)
-        
+            # col2.plotly_chart(px.pie(users, values='_id', names='is_subscribed', color_discrete_sequence=["pink", "purple"][::-1]), use_container_width=True)
+            # col3.subheader("User " + str(i+3))
+            hasClicked = card(
+        title="User " + str(i+3),
+        text="Some description",
+        # image="http://placekitten.com/200/300",
+        url="https://github.com/gamcoh/st-card"
+        )
+            # col3.plotly_chart(px.pie(users, values='_id', names='is_subscribed', color_discrete_sequence=["pink", "purple"][::-1]), use_container_width=True)
+            # col4.subheader("User " + str(i+4))
+            hasClicked = card(
+        title="User " + str(i+4),
+        text="Some description",
+        # image="http://placekitten.com/200/300",
+        url="https://github.com/gamcoh/st-card"
+        )
+            # col4.plotly_chart(px.pie(users, values='_id', names='is_subscribed', color_discrete_sequence=["pink", "purple"][::-1]), use_container_width=True)
+            # col5.subheader("User " + str(i+5))
+            hasClicked = card(
+        title="User " + str(i+5),
+        text="Some description",
+        # image="http://placekitten.com/200/300",
+        url="https://github.com/gamcoh/st-card"
+        )
+            # col5.plotly_chart(px.pie(users, values='_id', names='is_subscribed', color_discrete_sequence=["pink", "purple"][::-1]), use_container_width=True)
         # years,country=helper.country_year_list(users)
         
         # selected_year=st.sidebar.selectbox("Select Year",years)
